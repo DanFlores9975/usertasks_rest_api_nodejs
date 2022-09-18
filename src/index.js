2a4d3941fd33a385aa35c7fc65a97be48d9661c3
Dan Abisai Flores Hernandez <112905240+DanFlores9975@users.noreply.github.com>
2022-09-18 02:44:33 -0500

    Documentación de endpoints con swagger

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require('body-parser');
+//para documentar los endpoints con swagger
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const path = require('path');
+
+swaggerSpec = {
+  definition:{
+    openapi: "3.0.0",
+    info:{
+      title: "Rest API with nodeJS & express",
+      version: "1.0.0"
+    }
+  },
+  apis:[`${path.join(__dirname, "./routes/*.js")}`],
+};
+
 const app = express();
 
 //Configuracion del servidor.
@@ -8,7 +25,9 @@ app.set("port", process.env.PORT || 3000); //Asignamos el puerto con el que se t
 //middlewares
 app.use(morgan("dev")); //para ver de donde provienen y cuales son las peticiones.
 app.use(express.json()); //Para Poder utlizar archivos json en la API.
-app.use(express.urlencoded({ extended: false })); //para leer los datos simples para posterior procesarlos.
+app.use(bodyParser.urlencoded({extended:true})); //para leer los datos simples para posterior procesarlos.
+app.use('/api-doc',swaggerUI.serve,swaggerUI.setup(swaggerJsDoc(swaggerSpec)));//Declaramos la ruta donde se mostrará la documentación
+
 
 //Rutas ordenadas en un archivo.
 app.use("/api/task", require("./routes/routes.js"));
